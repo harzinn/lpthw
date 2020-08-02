6cb9cac8a00f23c9bb6b78ba741542734af54c2a
harzinn <nickkrugz@gmail.com>
2020-08-02 19:27:09 -0400

    fix for example 13 back to original

--- ex13.py
+++ ex13.py
@@ -1,8 +1,12 @@
 from sys import argv
 # read the WYSS section for how to run this
-script, lastname = argv
+script, lastname, second, third, fourth = argv
 
-name = input("What is your first name?")
+name = input("What is your name?")
 
 print(f"Hi, {name}!")
-print(f"I suppose your full name would be {name} {lastname} wouldn't it?")
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is", second)
+print("Your third variable is", third)
+print("Your 4th variable is", fourth)
