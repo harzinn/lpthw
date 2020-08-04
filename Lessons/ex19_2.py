a5aa61bcdf51943f8cde9095b5d7eed05afa179d
harzinn <nickkrugz@gmail.com>
2020-08-04 14:16:25 -0400

    Ex 19 review

--- Lessons/ex19_2.py
+++ Lessons/ex19_2.py
@@ -1,3 +1,8 @@
+def program():
+    print("Test")
+
+program()
+
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have {cheese_count} cheeses!")
     print(f"You have {boxes_of_crackers} boxes of crackers!")
