44afa703351f824850af4c2ea15b1cc0b193abd6
harzinn <nickkrugz@gmail.com>
2020-08-06 22:57:59 -0400

    Functions test in sandbox

--- sandbox.py
+++ sandbox.py
@@ -3,11 +3,13 @@ def magic(arg1):
     max = 180
     shards = max / 5
     shards1 = shards * 90
-    total = characters * shards1
+    total = arg1 * shards1
     return total
 
 characters = int(input("How many characters are we checking?\n"))
-
 howmany = magic(characters)
+print(f"You would recieve {howmany} ultimus orb shards.\m")
 
-print(f"You would recieve {howmany} ultimus orb shards.")
+characters2 = int(input("Again, How many characters are we checking?\n"))
+howmany2 = magic(characters2)
+print(f"You would recieve {howmany2} ultimus orb shards.\m")
