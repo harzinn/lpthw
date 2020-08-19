b68ce534fee789a5a02eacb94e9db518c5972c85
Nicholas Kruger <nickkrugz@gmail.com>
2020-08-19 13:17:56 -0400

    Sandbox update for unpacking ranges instead of ex33 while loop

--- sandbox.py
+++ sandbox.py
@@ -1,22 +1,21 @@
-#calling functions practice and improvements Friday, August 7, 2020 2:46 PM
+def program(max, steps):
+    i = 0
+    numbers = []
 
-#where the magic happens
-def magic(arg1):
-    #check how many characters we are going to multiply the final total by
-    characters = int(input("How many characters are we checking?\n"))
-    #take the input of the function and run ultimus math on it
-    x = arg1 / 5
-    total = x * 90
-    #take the total and then multiply it by the earlier character check
-    real_total = total * characters
-    #return the final total back to whatever called the function
-    return real_total
+    print(f"At the top i is {i}")
+    numbers = [*range(0, max, steps)]
 
-#Take input from user on shard information
-x = int(input("How many extra char shards will you recieve?\n"))
-#pass input from user into magic fucntion and save as y
-y = magic(x)
-#print the results of the funcion return in the next line
-print(f"You would recieve {y} ultimus orb shards.\n")
-#poor attempt at letting the user read it before closing if ran double clicked
-input("Press Enter to close")
+    i = i + steps
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+
+    print("The numbers: ")
+
+    for x in numbers:
+        print(x)
+
+top_end = int(input("Max range number:> "))
+increment_by = int(input("Increment by how many?:> "))
+
+program(top_end, increment_by)
