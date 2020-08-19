4dd34dce920b4b0829d6e320527c2633759c663a
Nicholas Kruger <nickkrugz@gmail.com>
2020-08-19 11:50:20 -0400

    Exercise 33 and Practice

---
+++ Lessons/ex33/ex33_test.py
@@ -0,0 +1,23 @@
+
+
+def program():
+    increment = 2
+    max = 10
+    i = 0
+    numbers = []
+
+    while i < max:
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        i = i + increment
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+
+
+    print("The numbers: ")
+
+    for x in numbers:
+        print(x)
+
+program()
